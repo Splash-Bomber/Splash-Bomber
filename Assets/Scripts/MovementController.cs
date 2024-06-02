8f9241d6d3a7bc7c192a46b82d1cc64480f51935
SeongHwan <34639547+osh0678@users.noreply.github.com>
2024-06-02 13:30:19 +0900

    feat: 게임매니저 추가로 인해 게임 순환할 수 있도록 기능 추가

--- Assets/Scripts/MovementController.cs
+++ Assets/Scripts/MovementController.cs
@@ -97,5 +97,6 @@ public class MovementController : MonoBehaviour
     private void OnDeathSequenceEnded()
     {
         gameObject.SetActive(false);
+        FindObjectOfType<GameManager>().CheckWinState();
     }
 }
