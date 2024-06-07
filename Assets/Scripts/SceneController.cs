567b7fedf2125395171ceeb15f0cd7023232237e
SeongHwan <34639547+osh0678@users.noreply.github.com>
2024-06-08 02:17:57 +0900

    refactor : scene 전환 수정

--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -4,16 +4,19 @@ using UnityEngine.SceneManagement;
 
 public class SceneController : MonoBehaviour
 {
+     public string anyKeySceneName; // 키 입력 시 전환할 씬 이름을 저장할 변수
+    
+        void Update()
+        {
+            // 키 입력을 감지하여 씬을 전환합니다.
+            if (Input.anyKey)
+            {
+                SceneManager.LoadScene(anyKeySceneName);
+            }
+        }
+    
     public void ChangeScene(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
-    }
-    
-    public void AnyKey(string sceneName)
-    {
-        if (Input.anyKey)
-        {
-            SceneManager.LoadScene(sceneName);
-        }
     }
 }
